d10d0104cafbf38a86789feec1e621ca2feff347
HassanMD-Dev <muhayyudinhassan@gmail.com>
2025-11-30 12:40:03 +0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let count = 0;
+let plusBtn = document.querySelector(".plus");
+let minusBtn = document.querySelector(".minus");
+let output = document.querySelector("#output");
+let reset = document.querySelector("#reset");
+
+plusBtn.addEventListener("click", () => {
+    count++;
+    output.innerText = count;
+});
+
+minusBtn.addEventListener("click", () => {
+    count--;
+    output.innerText = count;
+})
+reset.addEventListener("click", () => {
+    count = 0;
+    output.innerText = count;
+})
