caa8bef2ba27730a14c86372fca37456f4e79cf6
Hassan Muhayyudin <muhayyudinhassan@gmail.com>
2025-12-06 17:27:28 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,17 @@ let output = document.querySelector("#output");
 let reset = document.querySelector("#reset");
 
 plusBtn.addEventListener("click", () => {
-  count++;
-  output.innerText = count;
+    count++;
+    output.innerText = count;
+    
+    // Spin the button every time it's clicked
+    animate(plusBtn, { rotate: "+=360" }, { duration: 0.5 });
 });
 minusBtn.addEventListener("click", () => {
   count--;
   output.innerText = count;
+
+animate(plusBtn, { rotate: "+=360" }, { duration: 0.5 });
 });
 
 reset.addEventListener("click", () => {
